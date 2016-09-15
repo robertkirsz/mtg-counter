388aa9e7ff263e9aad04d0c1a9b75fd7c3cdcc6d
Robert Kirsz <robert.kirsz@gmail.com>
2016-09-15 23:50:28 +0200

    Settings panel now closes automatically

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -1,8 +1,10 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../actions/fuelSavingsActions'
 import cn from 'classnames'
+
+import { bindMethods, isContainedIn } from '../utils';
 
 function mapStateToProps({ gameState }) {
   return {
@@ -16,52 +18,79 @@ function mapDispatchToProps(dispatch) {
   }
 }
 
-const SettingsPanel = ({ actions, settingsPanel }) => {
-  const cogIconClick = () => {
-    actions.settingsPanel('toggle')
+class SettingsPanel extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    bindMethods(this)
   }
 
-  const settingsPanelIconClick = (iconType) => {
-    if (iconType === 'poison') actions.showCounters('poison')
-    if (iconType === 'commander') actions.showCounters('commander')
+  componentDidMount () {
+    window.addEventListener('click', this.windowClick);
   }
 
-  return (
-    <div className={cn(
-      'settings',
-      { 'settings--opened': settingsPanel }
-    )}>
-      <div className="settings__icons-wrapper">
-        <span
-          className="settings__icon poison"
-          onClick={() => { settingsPanelIconClick('poison'); }}
-          title="Poison counter"
-        />
-        <span
-          className="settings__icon commander"
-          onClick={() => { settingsPanelIconClick('commander'); }}
-          title="Commander damage"
-        />
-        <span
-          className="settings__icon dice"
-          onClick={() => { settingsPanelIconClick('dice'); }}
-          title="Dice"
-        />
-        <span
-          className="settings__icon fa fa-undo"
-          onClick={() => { settingsPanelIconClick('reset'); }}
-          title="Reset current game"
-        />
+  componentWillUnmount () {
+    window.removeEventListener('click', this.windowClick);
+  }
+
+  cogIconClick () {
+    this.props.actions.settingsPanel('toggle')
+  }
+
+  settingsPanelIconClick (iconType) {
+    if (iconType === 'poison') this.props.actions.showCounters('poison')
+    if (iconType === 'commander') this.props.actions.showCounters('commander')
+    if (iconType === 'reset') console.log('reste')
+  }
+
+  windowClick (e) {
+    const clickedOut = !isContainedIn(e.target, this.refs.settingPanel);
+    if (this.props.settingsPanel && clickedOut) this.props.actions.settingsPanel('toggle')
+  }
+
+  render () {
+    return (
+      <div
+        className={cn(
+          'settings',
+          { 'settings--opened': this.props.settingsPanel }
+        )}
+        ref='settingPanel'
+      >
+        <div className="settings__icons-wrapper">
+          <span
+            className="settings__icon poison"
+            onClick={() => { this.settingsPanelIconClick('poison'); }}
+            title="Poison counter"
+          />
+          <span
+            className="settings__icon commander"
+            onClick={() => { this.settingsPanelIconClick('commander'); }}
+            title="Commander damage"
+          />
+          {/*
+            <span
+              className="settings__icon dice"
+              onClick={() => { this.settingsPanelIconClick('dice'); }}
+              title="Dice"
+            />
+          */}
+          <span
+            className="settings__icon fa fa-undo"
+            onClick={() => { this.settingsPanelIconClick('reset'); }}
+            title="Reset current game"
+          />
+        </div>
+        <span className="fa fa-cog" onClick={this.cogIconClick} />
       </div>
-      <span className="fa fa-cog" onClick={cogIconClick} />
-    </div>
-  )
+    )
+  }
 }
 
 SettingsPanel.propTypes = {
   actions: PropTypes.object.isRequired,
   settingsPanel: PropTypes.bool.isRequired
-};
+}
 
 export default connect(
   mapStateToProps,
