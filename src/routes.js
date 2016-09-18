93d44bfc81f79f8963b6719b39dc7e2e5f6a5039
Robert Kirsz <robert.kirsz@gmail.com>
2016-09-18 12:55:11 +0200

    Clean-up

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import { App, HomePage, NotFoundPage } from './components';
+import { App, HomePage/* , NotFoundPage */ } from './components';
 // import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
 // import AboutPage from './components/AboutPage.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="*" component={NotFoundPage} />
+    {/* <Route path="*" component={NotFoundPage} /> */}
     {/* <Route path="fuel-savings" component={FuelSavingsPage} /> */}
     {/* <Route path="about" component={AboutPage} /> */}
   </Route>
