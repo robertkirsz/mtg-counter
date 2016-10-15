1b19336c99f57ca7e353ffd33e8f6452f85fd83c
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-15 18:09:52 +0200

    Updated actions tests

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,18 +1,18 @@
 import { expect } from 'chai'
-import { settingsPanel } from './'
+import { changeScreen } from './'
 
 describe('Redux action', () => {
   it('should be a function', () => {
-    expect(settingsPanel).to.be.a('function')
+    expect(changeScreen).to.be.a('function')
   }),
   it('should return a type', () => {
-    expect(settingsPanel().type).to.exist
+    expect(changeScreen().type).to.exist
   }),
   it('type should a string', () => {
-    expect(settingsPanel().type).to.be.a('string')
+    expect(changeScreen().type).to.be.a('string')
   }),
   it('should return passed values', () => {
     const input = 'ABC'
-    expect(settingsPanel(input).action).to.equal(input)
+    expect(changeScreen(input).screenName).to.equal(input)
   })
 })
