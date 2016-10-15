ddb2c9ac1060e2027c716a93d88720d464cb7729
Robert Kirsz <robert.kirsz@gmail.com>
2016-10-15 17:21:27 +0200

    Settings panel can now be closed with swipe

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -4,8 +4,9 @@ import { bindActionCreators } from 'redux'
 import * as actions from '../actions'
 import cn from 'classnames'
 import _ from 'lodash'
+import Swipeable from 'react-swipeable'
 
-import { bindMethods, isContainedIn } from '../utils';
+import { bindMethods } from '../utils'
 
 function mapStateToProps({ gameState }) {
   return {
@@ -20,6 +21,8 @@ function mapDispatchToProps(dispatch) {
   }
 }
 
+const debugMode = false
+
 class SettingsPanel extends Component {
   constructor(props, context) {
     super(props, context)
@@ -28,18 +31,22 @@ class SettingsPanel extends Component {
   }
 
   componentDidMount () {
-    window.addEventListener('click', this.windowClick);
+    window.addEventListener('click', this.windowClick)
   }
 
   componentWillUnmount () {
-    window.removeEventListener('click', this.windowClick);
+    window.removeEventListener('click', this.windowClick)
   }
 
+  // Handles clicking on the cog icon
   cogIconClick () {
+    if (debugMode) console.warn('SettingsPanel -> cogIconClick()')
     this.props.actions.settingsPanel('toggle')
   }
 
+  // Handles clicking on the settings icons
   settingsPanelIconClick (iconType) {
+    if (debugMode) console.warn('SettingsPanel -> settingsPanelIconClick(iconType)', iconType)
     const { actions, players } = this.props
     if (iconType === 'poison') actions.showCounters('poison')
     if (iconType === 'commander') actions.showCounters('commander')
@@ -71,63 +78,73 @@ class SettingsPanel extends Component {
     }
   }
 
-  windowClick (e) {
-    const {
-      props: { settingsPanel, actions },
-      refs: { settingsPanelNode }
-    } = this
+  // Fires when clicking anywhere on the screen
+  // (top element has 'stopPropagation' on it so it won't fire this function)
+  windowClick () {
+    if (debugMode) console.warn('SettingsPanel -> winclick()')
+    if (this.props.settingsPanel) this.closeSettingsPanel()
+  }
 
-    const clickedOut = !isContainedIn(e.target, settingsPanelNode)
-    if (settingsPanel && clickedOut) actions.settingsPanel('close')
+  // Sends action to close settings panel
+  closeSettingsPanel () {
+    if (debugMode) console.warn('SettingsPanel -> closeSettingsPanel()')
+    this.props.actions.settingsPanel('close')
   }
 
   render () {
     const { players, hidden, settingsPanel } = this.props
-
-    const allPlayersAreUndefined = _.every(players, player => !player.isDefined());
-    const settingsPanelClassName = cn('settings', { 'settings--opened': settingsPanel, 'settings--hidden': hidden })
+    // True if none of the players is fully initialized
+    const allPlayersAreUndefined = _.every(players, player => !player.isDefined())
+    // Container classes
+    const settingsPanelClassName = cn(
+      'settings', // Default class
+      {
+        'settings--opened': settingsPanel, // Opens/closes setting panel
+        'settings--hidden': hidden // Hides everything
+      })
 
     return (
-      <div
+      <Swipeable
         className={settingsPanelClassName}
-        ref="settingsPanelNode"
+        onSwipedRight={this.closeSettingsPanel}
+        onClick={e => e.stopPropagation()} // Prevents 'widnowClick()'
       >
         <div className="settings__icons-wrapper">
           <span
             className="settings__icon poison"
-            onClick={() => { this.settingsPanelIconClick('poison'); }}
+            onClick={() => { this.settingsPanelIconClick('poison') }}
             title="Poison counter"
             disabled={allPlayersAreUndefined}
           />
           <span
             className="settings__icon commander"
-            onClick={() => { this.settingsPanelIconClick('commander'); }}
+            onClick={() => { this.settingsPanelIconClick('commander') }}
             title="Commander damage"
             disabled={allPlayersAreUndefined}
           />
           <span
             className="settings__icon dice"
-            onClick={() => { this.settingsPanelIconClick('dice'); }}
+            onClick={() => { this.settingsPanelIconClick('dice') }}
             title="Dice"
           />
           <span
             className="settings__icon fa fa-paint-brush"
-            onClick={() => { this.settingsPanelIconClick('change-color'); }}
+            onClick={() => { this.settingsPanelIconClick('change-color') }}
             title="Change color"
           />
           <span
             className="settings__icon fa fa-undo"
-            onClick={() => { this.settingsPanelIconClick('reset'); }}
+            onClick={() => { this.settingsPanelIconClick('reset') }}
             title="Reset current game"
           />
           <span
             className="settings__icon fa fa-file-o"
-            onClick={() => { this.settingsPanelIconClick('new-game'); }}
+            onClick={() => { this.settingsPanelIconClick('new-game') }}
             title="Start new game"
           />
         </div>
         <span className="fa fa-cog" onClick={this.cogIconClick} />
-      </div>
+      </Swipeable>
     )
   }
 }
